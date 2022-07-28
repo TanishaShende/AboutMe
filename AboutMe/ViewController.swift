edda2e058ba3c467574cabc5d18297172f771cb3
Tanisha Shende <tshende316@gmail.com>
2022-07-28 12:20:19 -0400

    Added labels, image, and button.

--- AboutMe/ViewController.swift
+++ AboutMe/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var fact: UILabel!
+    
+    let facts = ["I love reading and writing!", "I love space, math, and CS!", "I want to be a polyglot one day.", "I want to be a published writer one day.", "My favorite movie is The Imitation Game.", "I'm really into machine learning and artificial intelligence right now!", "I love classics, art history, and philosophy."]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        let randomNumber = Int.random(in: 0...(facts.count-1))
+        
+        fact.text = facts[randomNumber]
+    }
+    
 }
 
